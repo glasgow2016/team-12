19bfa729243ba2ad23e37506aafe7b0be8703797
eliriel <borislav.hristov.147@gmail.com>
2016-11-05 20:06:48 +0000

    Continueing(No spell check) working on game section

--- HelixProject/helixproject/helixapp/urls.py
+++ HelixProject/helixproject/helixapp/urls.py
@@ -4,5 +4,7 @@ from helixapp import views
 urlpatterns = patterns('',
         url(r'^$', views.index, name='index'),
 		url(r'^game/$', views.game, name='game'),
+		url(r'^game/new/$', views.gameNew, name='game'),
+		url(r'^game/riddle/$', views.gameRiddle, name='game'),
+		url(r'^game/instructions/$', views.gameInstructions, name='game'),
 	)
-
--- HelixProject/helixproject/helixapp/views.py
+++ HelixProject/helixproject/helixapp/views.py
@@ -4,10 +4,21 @@ from django.http import HttpResponse
 
 def index(request):
     if request.method=='POST':
-	return render(request,'helixapp/index.html')
+		return render(request,'helixapp/index.html')
     else:
-	return render(request,'helixapp/index.html')
+		return render(request,'helixapp/index.html')
 
 	
 def game(request):
 	return render(request,'helixapp/game.html')
+	
+def gameInstructions(request):
+	return render(request,'helixapp/game/instructions.html')
+
+def gameNew(request):
+	return render(request,'helixapp/game/new.html')
+
+def gameRiddle(request):
+	return render(request,'helixapp/game/riddle.html')
+	
+	
