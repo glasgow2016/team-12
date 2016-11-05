2393d16e8dd24df305576c1adbb59f08cc0c482d
eliriel <borislav.hristov.147@gmail.com>
2016-11-05 23:08:08 +0000

    Riddle finished, design needed

--- HelixProject/helixproject/helixapp/views.py
+++ HelixProject/helixproject/helixapp/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.http import HttpResponse
 from helixapp.models import tree
+import random
 
 pc=0
+questDict={"1": "Which is the device that has a camera", "2": "Which is the device that doesn't have a camera"}
+
+savedRiddles={}
 
 def index(request):
     global pc
@@ -16,6 +20,7 @@ def index(request):
         response.set_cookie(str(pc), pc)
     return response
 
+	
 def getTree():
     for t in tree.objects.all():
         if t.isTaken==False:
@@ -24,16 +29,21 @@ def getTree():
             break
     return t
 	
+	
 def game(request):
 	return render(request,'helixapp/game.html')
+	
 	
 def game_instructions(request):
 	return render(request,'helixapp/game/instructions.html')
 
+	
+	
 def game_new(request):
-
-                
-	return render(request,'helixapp/game/riddle.html')
+	global questDict
+	x = random.choice(questDict.keys())
+	context_dict = {"code": 123321,"riddle": questDict[x]}
+	return render(request,'helixapp/game/riddle.html',context_dict)
 
 def game_riddle(request):
 	return render(request,'helixapp/game/riddle.html')
